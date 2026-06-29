7259aa864a88dce9ad01869dfdf308ac871e9268
Vitor Picolotto <picolottovitor@gmail.com>
2026-06-29 12:44:06 -0300

    implementando o método post

--- js/api.js
+++ js/api.js
@@ -11,9 +11,23 @@ const api = {
             throw error;
         }
     },
-
-
-
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }, 
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+            }
+            
+            catch {
+                alert('Erro ao adicionar pensamentos')
+                throw error;
+            }
+    }
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    ui.renderizarPensamentos()
+    ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById('pensamento-form');
+    formularioPensamento.addEventListener('submit', manipularSubmissaoFormulario)
+
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById('pensamento-id').value;
+    const conteudo = document.getElementById('pensamento-conteudo').value;
+    const autoria = document.getElementById('pensamento-autoria').value
+
+    try {
+        await api.salvarPensamento({conteudo, autoria});
+        ui.renderizarPensamentos
+    } catch (error) {
+        alert('Erro ao salvar pensamento!')
+    }
+}
+
 
